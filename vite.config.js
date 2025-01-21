51e70136caf2d7ec109c8e1c57784a8b8b2f9c1f
Keshav Bansal <keshav@malwarekosh.org>
2025-01-21 12:47:24 +0530

    remove server port

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // This allows Vite to bind to all available network interfaces
-    port: 3000, // Optional: Change the port if needed
-  },
+
 })
